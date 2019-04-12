3f790792ab639e23aebe1f2b912595ebca407f16
sankalpsmehra123 <sankalpsmehra123@gmail.com>
2019-04-12 21:58:36 +0530

    XGBoost done (not running on atom)

--- XGBoost_reg.py
+++ XGBoost_reg.py
@@ -1,11 +1,10 @@
 # Platform: `linux(Ubuntu 18.04)'
 # python version: 3.6
-# predicting SDN traffic using Random Forest
+# predicting SDN traffic using XGBoost
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import pandas as pd
-from math import sqrt
 
 # Importing the dataset
 dataset = pd.read_csv(
@@ -18,41 +17,31 @@ y = dataset.iloc[:, 4].values
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=0)
 
-# Fitting Random Forest Regression to the dataset
-regressor = RandomForestRegressor(n_estimators=100, random_state=0)
-regressor.fit(x_train, y_train)
+# Fitting XGBoost Regression to the dataset
+xgbr = xgb.XGBRegressor()
+regressor = xgbr.fit(x_train, y_train)
 
 # predictions
 y_pred = regressor.predict(x_test)
-error = y_pred - y_test
-error_rms = 0
-sum = 0
-for i in error:
-    sum += (i**2)
-sum /= len(error)
-print(len(error))
-print(sum)
-error_rms = sqrt(sum)
-print(error_rms)
+
 # seprating seconds form the Dataset
 sec = x_test[:, 0]
 
-
-# Visualizing Random Forest for the dataset
+# Visualizing XGBoost for the dataset
 plt.scatter(sec, y_pred, color='blue')
 plt.savefig(
     '/home/shadowziyus/Documents/'
-    'SDN-Traffic-Prediction-Through-Machine-Learning/random_forest_predicted.png')
+    'SDN-Traffic-Prediction-Through-Machine-Learning/XGB_predicted.png')
 plt.show()
 plt.scatter(sec, y_test, color='red')
 plt.savefig(
     '/home/shadowziyus/Documents/'
-    'SDN-Traffic-Prediction-Through-Machine-Learning/random_forest_actual.png')
+    'SDN-Traffic-Prediction-Through-Machine-Learning/XGB_actual.png')
 plt.show()
 
 error = y_pred - y_test
 plt.scatter(sec, error, color='black')
 plt.savefig(
     '/home/shadowziyus/Documents/'
-    'SDN-Traffic-Prediction-Through-Machine-Learning/random_forest_error.png')
+    'SDN-Traffic-Prediction-Through-Machine-Learning/XGB_error.png')
 plt.show()
